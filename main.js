34593d6e646654cc41cc4cc9863435a1fffe4f1a
anthony <anthony@anthony.vbox>
2022-03-08 23:59:26 +0300

    canvas layout fix, reset button

--- main.js
+++ main.js
@@ -1,33 +1,27 @@
+// todo undo 
+// todo save - LocalStorage, Save and Load drawing. JSON.stringify and JSON.parse
 var canvas = document.getElementById("main_canvas");
 var context = canvas.getContext("2d");
 
 var color_selector_obj = document.getElementById("color_selector");
+var reset_button_obj = document.getElementById("reset");
 
 var window_width = window.innerWidth;
 var window_height = window.innerHeight;
 
-var canvas_width = window_width * 0.8;
-var canvas_height = window_height * 0.8
-var canvas_width_step = window_width * 0.04;
-var canvas_height_step = window_height * 0.04;
+var canvas_step = 30;
+var canvas_width = Math.floor(window_width*0.8/canvas_step)*canvas_step;
+var canvas_height = Math.floor(window_height*0.8/canvas_step)*canvas_step;
 
-canvas.width = canvas_width;
-canvas.height = canvas_height;
+main()
 
-for (var x = 1; x < canvas_width; x += canvas_width_step) {
-  context.moveTo(x, 0);
-  context.lineTo(x, canvas_height);
+function main() {
+    canvas.width = canvas_width;
+    canvas.height = canvas_height;
+    reset_canvas();
+    canvas.addEventListener("click", change_color, false);
+    reset_button_obj.addEventListener("click", reset_canvas, false);
 }
-
-for (var y = 1; y < canvas_height; y += canvas_height_step) {
-  context.moveTo(0, y);
-  context.lineTo(canvas_width, y);
-}
-
-context.strokeStyle = "Black";
-context.stroke();
-
-canvas.addEventListener("click", change_color, false);
 
 function Cell(row, column) {
     this.row = row;
@@ -38,7 +32,7 @@ function change_color(pos) {
     var cell = getCursorPosition(pos);
     var color = color_selector_obj.value;
     context.beginPath();  
-    context.rect(cell.column * canvas_width_step, cell.row * canvas_height_step, canvas_width_step, canvas_height_step);
+    context.rect(cell.column*canvas_step, cell.row*canvas_step, canvas_step, canvas_step);
     context.fillStyle = color;
     context.fill();
 };
@@ -58,9 +52,23 @@ function getCursorPosition(pos) {
 
     x -= canvas.offsetLeft;
     y -= canvas.offsetTop;
-    var cell = new Cell(Math.floor(y/canvas_height_step), Math.floor(x/canvas_width_step));
-    console.log(x);
-    console.log(y);
+    var cell = new Cell(Math.floor(y/canvas_step), Math.floor(x/canvas_step));
     return cell;
 };
 
+function reset_canvas() {
+    canvas.width = canvas.width;
+
+    for (var x = 0; x < canvas_width; x += canvas_step) {
+        context.moveTo(x, 0);
+        context.lineTo(x, canvas_height);
+    }
+      
+    for (var y = 0; y < canvas_height; y += canvas_step) {
+        context.moveTo(0, y);
+        context.lineTo(canvas_width, y);
+    }
+      
+    context.strokeStyle = "Black";
+    context.stroke();
+};
